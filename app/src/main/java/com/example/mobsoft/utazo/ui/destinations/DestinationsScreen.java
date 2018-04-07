86ac1985453840d77b7907cdc91de6a6e025892d
Kovacs Adam <adaam.ko@gmail.com>
2018-04-07 21:14:01 +0200

    Added Screens

---
+++ app/src/main/java/com/example/mobsoft/utazo/ui/destinations/DestinationsScreen.java
@@ -0,0 +1,11 @@
+package com.example.mobsoft.utazo.ui.destinations;
+
+import java.util.List;
+
+public interface DestinationsScreen {
+    void showDestinations();
+
+    void showNetworkError();
+
+    void AddDestination();
+}
